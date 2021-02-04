aea7fb432981cd5a067bff3e0d4b789df1f157c8
EagleVee <nnavu1@gmail.com>
2021-02-04 12:58:18 +0700

    fix main

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,73 @@
 import 'package:flutter/material.dart';
-import 'containers/HomePage.dart';
+import 'package:mealnote/utils/DBHelper.dart';
+import 'package:mealnote/containers/NoteList.dart';
+import 'package:mealnote/containers/NotePage.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: "Simple Note App",
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var db = new DBHelper();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          leading: Container(
+              padding: EdgeInsets.all(8.0),
+              child:
+                  Image.asset("package:mealnote/assets/images/note-icon.png")),
+          title: Text("Notes App",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.w300)),
+          backgroundColor: Colors.blue,
+        ),
+        floatingActionButton: FloatingActionButton(
+          tooltip: 'Increment',
+          child: Icon(Icons.edit, color: Colors.white),
+          onPressed: () => Navigator.of(context).push(MaterialPageRoute(
+              builder: (BuildContext context) => new NotePage(null, true))),
+        ), // This trailing comma makes auto-formatting nicer for build methods.
+        backgroundColor: Colors.grey[200],
+        body: FutureBuilder(
+          future: db.getNote(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              print(snapshot.error);
+            }
+            var data = snapshot.data;
+
+            return snapshot.hasData
+                ? new NoteList(data)
+                : Center(
+                    child: Text("No Data"),
+                  );
+          },
+        ));
+  }
+}
